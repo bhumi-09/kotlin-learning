3d685fdd370313730c57624a96f3f872f3630705
bhumi-09 <bhumikakshejwal@gmail.com>
2025-07-22 15:42:19 +0530

    Output in kotlin

---
+++ 04-Input-Output/output.kt
@@ -0,0 +1,16 @@
+fun main() {
+   
+    print("Hello, ")                        //prints on the same line.
+    print("world!")
+    
+    // Using variables with println
+    val name = "Bhumika"
+    val age = 22
+
+    println("\n\n--- User Info ---")        //prints and moves to the next line.
+    println("Name: $name")
+    println("Age: $age")
+
+    // Using string concatenation
+    println("Next year, you will be " + (age + 1) + " years old.")
+}
