539b72c199e8407effa63e2cc9c10921220a237d
bhumi-09 <bhumikakshejwal@gmail.com>
2025-07-23 12:30:43 +0530

    Basic class in kotlin

---
+++ 05-Object-Oriented-Programming/01-Classes/class.kt
@@ -0,0 +1,10 @@
+class Demo{
+    fun sayHello(){
+        println("Hello, I'm Bhumika")
+    }
+}
+
+fun main(){
+    val obj = Demo();       //creating an instance of the class
+    obj.sayHello()          // calling the function
+}
